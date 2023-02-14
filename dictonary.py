fda54a9628f338fef21b972d962e678b3b2b2c8c
Mauricio Lecaros Aranis <maurilearn@gmail.com>
2023-02-14 18:05:59 -0300

    actualización dictonary.py

--- dictonary.py
+++ dictonary.py
@@ -7,3 +7,9 @@ print(elficio["nivel"])
 elficio["nivel"]=49
 print(elficio["nivel"])
 print(elficio.keys())
+print(elficio.values())
+
+#convertir un diccionario en una lista. funcion .items()
+print(list({"k1":5, "k2":7}.items()))
+#convertir lista en diccionario
+print(dict([('k1', 5), ('k2', 7)]))
